d5162297519ef6089fd04c6c8f1361baa9f4cd2e
saltscha <Simon.Altschaeffl@edvschule-plattling.de>
2026-01-14 09:29:21 +0100

    Init

---
+++ src/main/java/de/edvschuleplattling/irgendwieanders/IrgendwieAndersApplication.java
@@ -0,0 +1,13 @@
+package de.edvschuleplattling.irgendwieanders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrgendwieAndersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrgendwieAndersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/edvschuleplattling/irgendwieanders/IrgendwieAndersApplicationTests.java
@@ -0,0 +1,13 @@
+package de.edvschuleplattling.irgendwieanders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrgendwieAndersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
