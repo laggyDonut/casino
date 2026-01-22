7f621685c308a0a3cf331ac42bbcf6e3072f5cf9
dschuhba <dominik.schuhbaum@edvschule-plattling.de>
2026-01-22 15:17:57 +0100

    Simon nerven.

--- src/main/java/de/edvschuleplattling/irgendwieanders/model/Game.java
+++ src/main/java/de/edvschuleplattling/irgendwieanders/model/Game.java
@@ -11,4 +11,6 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    String mango;
+    //TODO Simon muss besser Programmieren und schneller
 }
