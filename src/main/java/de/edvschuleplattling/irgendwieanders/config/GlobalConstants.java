f5f1624abcee647d5c28b542bf10667e71fc1dbd
dschuhba <dominik.schuhbaum@edvschule-plattling.de>
2026-02-09 16:03:28 +0100

    Add GlobalConstants interface for application-wide constants

---
+++ src/main/java/de/edvschuleplattling/irgendwieanders/config/GlobalConstants.java
@@ -0,0 +1,19 @@
+package de.edvschuleplattling.irgendwieanders.config;
+
+public interface GlobalConstants {
+    // Allgemeine Konstanten
+    String APP_NAME = "IrgendwieAnders";
+    String APP_VERSION = "1.0.0";
+
+    // Sicherheitskonstanten
+    int PASSWORD_MIN_LENGTH = 12;
+    int PASSWORD_MAX_LENGTH = 255;
+
+    // Konstanten
+    String CURR_KEY = "EUR";
+    long CURR_TO_COINS = 100; // 1 EUR = 100 Coins
+
+    static long currToCoins(long amount) {
+        return CURR_TO_COINS*amount;
+    }
+}
